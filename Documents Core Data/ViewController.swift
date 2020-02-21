9ed7b79f6ba504399790c5cd7d1197257b267187
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2020-02-20 21:04:23 -0600

    First commit

---
+++ Documents Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documents Core Data
+//
+//  Created by Ante Plecas on 2/20/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
